88d671f9f051b105ce6f5f5525ad8cf9f18261b1
JCannon1 <jessecannon1@hotmail.com>
2017-02-23 15:33:24 -0500

    edited shows

--- shows.php
+++ shows.php
@@ -10,34 +10,26 @@
 
 <?php 
 
-session_start();
-
-if (!empty($_SESSION['userId'])) {
-    echo '<a href="select-network.php">Add a new Show</a>';
-}
-
 $conn = new PDO('mysql:host=ca-cdbr-azure-central-a.cloudapp.net;dbname=comp1006jessecannondatabase', 'bf3c946f4d66ff', '1d953141');
 
 $sql = "SELECT show_name, first_year, network_name FROM shows ORDER BY show_name";
 
 $cmd = $conn->prepare($sql);
 $cmd->execute();
-$albums = $cmd->fetchAll();
+$shows = $cmd->fetchAll();
 
 echo '<table class="table table-striped table-hover">
 <tr><th>Name</th><th>Year</th><th>Network</th>';
 
-echo '</tr>';
 
 foreach ($shows as $show) {
     echo '<tr><td>' . $show['show_name'] . '</td>
     <td>' . $show['first_year'] . '</td>
     <td>' . $show['network_name'] . '</td>';
 
-    echo '</tr>';
 }
 
-echo '</table>';
+
 
 $conn = null;
 ?>
